8a735148b0c38033d620133bdcb51c2d4f1db06c
vitowlabres <vitorlabres12@gmail.com>
2025-09-16 21:52:27 -0300

    ajuste em app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,20 @@
 import { Routes } from '@angular/router';
 import { ProductEntryComponent } from './components/product-entry/product-entry.component';
 import { UserRegistrationComponent } from './components/user-registration/user-registration.component';
+import { authGuard } from './guards/auth.guards';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'product-entry', component: ProductEntryComponent },
-  { path: 'user-registration', component: UserRegistrationComponent },
+  {
+    path: 'product-entry',
+    component: ProductEntryComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'user-registration',
+    component: UserRegistrationComponent,
+    canActivate: [authGuard],
+  },
   {
     path: 'login',
     loadComponent: () =>
