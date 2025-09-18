843e507b63fe3dd04316cf83d53f8f58fadf2c86
vitowlabres <vitorlabres12@gmail.com>
2025-09-17 23:14:04 -0300

    icone do dashboard e redirecionamento

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -47,8 +47,7 @@ export class LoginComponent {
         this.loginValid = true;
         this.loginSuccess = true;
         localStorage.setItem('accessToken', res.accessToken);
-        console.log('chegou até aqui');
-        this.router.navigate(['/product-entry']);
+        this.router.navigate(['/dashboard']);
       },
       error: (err: any) => {
         console.error('Erro no login:', err);
