2390a26c85e37f12d5d87a0872569e6b3d77b8b9
Rafael Brum Fuchs <brum.fuchs@gmail.com>
2025-09-20 15:06:19 -0300

    correções na dashboard

--- backend/src/dashboard/dashboard.service.ts
+++ backend/src/dashboard/dashboard.service.ts
@@ -5,7 +5,8 @@ export class DashboardService {
   getMetrics() {
     return {
       bar: Array.from({ length: 4 }, () => Math.floor(Math.random() * 100)),
-      line: Array.from({ length: 4 }, () => Math.floor(Math.random() * 100))
+      line: Array.from({ length: 4 }, () => Math.floor(Math.random() * 100)),
+      labels: ['sem1', 'sem2', 'sem3', 'sem4']
     };
   }
 }
--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -5,6 +5,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import Chart from 'chart.js/auto';
 import { Subscription, interval } from 'rxjs';
+// import { DashboardService } from './dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -32,7 +33,7 @@ export class DashboardComponent implements AfterViewInit {
       data: {
         labels: ['sem1', 'sem2', 'sem3', 'sem4'],
         datasets: [{
-          data: [65, 59, 80, 81],
+          data: [],
           backgroundColor: '#2b891f'
         }]
       },
@@ -50,10 +51,10 @@ export class DashboardComponent implements AfterViewInit {
     this.lineChart = new Chart(this.lineCanvas.nativeElement, {
       type: 'line',
       data: {
-        labels: ['Semana 1', 'Semana 2', 'Semana 3', 'Semana 4'],
+        labels: ['sem1', 'sem2', 'sem3', 'sem4'],
         datasets: [{
           label: 'Produção',
-          data: [65, 59, 80, 81],
+          data: [],
           borderColor: '#2b891f',
           backgroundColor: 'rgba(43,137,31,0.1)',
           fill: true,
@@ -75,35 +76,35 @@ export class DashboardComponent implements AfterViewInit {
       }
     });
 
-    // this.updateSub = interval(5000).subscribe(() => {
-    //   const novosDadosBar = Array.from({ length: 4 }, () => Math.floor(Math.random() * 100));
-    //   const novosDadosLine = Array.from({ length: 4 }, () => Math.floor(Math.random() * 100));
-    //   this.chart.data.datasets[0].data = novosDadosBar;
-    //   this.chart.update();
-    //   this.lineChart.data.datasets[0].data = novosDadosLine;
-    //   this.lineChart.update();
-    // });
-
-    // Atualiza os gráficos a cada 10 segundos (10000 ms)
-    this.updateSub = interval(10000).subscribe(() => {
-      this.dashboardService.getMetrics().subscribe(data => {
-        // Atualiza os dados do gráfico de barras
-        this.chart.data.labels = data.labels;
-        this.chart.data.datasets[0].data = data.bar;
-        this.chart.update();
-
-
-        // Atualiza os dados do gráfico de linha
-        this.lineChart.data.labels = data.labels;
-        this.lineChart.data.datasets[0].data = data.line;
-        this.lineChart.update();
-      });
+    this.updateSub = interval(5000).subscribe(() => {
+      const novosDadosBar = Array.from({ length: 4 }, () => Math.floor(Math.random() * 100));
+      const novosDadosLine = Array.from({ length: 4 }, () => Math.floor(Math.random() * 100));
+      this.chart.data.datasets[0].data = novosDadosBar;
+      this.chart.update();
+      this.lineChart.data.datasets[0].data = novosDadosLine;
+      this.lineChart.update();
     });
 
-    ngOnDestroy() {
-    // Cancela a assinatura ao destruir o componente
-      this.updateSub?.unsubscribe();
-    }
+    // Atualiza os gráficos a cada 10 segundos (10000 ms)
+    // this.updateSub = interval(10000).subscribe(() => {
+    //   this.dashboardService.getMetrics().subscribe(data => {
+    //     // Atualiza os dados do gráfico de barras
+    //     this.chart.data.labels = data.labels;
+    //     this.chart.data.datasets[0].data = data.bar;
+    //     this.chart.update();
+
+
+    //     // Atualiza os dados do gráfico de linha
+    //     this.lineChart.data.labels = data.labels;
+    //     this.lineChart.data.datasets[0].data = data.line;
+    //     this.lineChart.update();
+    //   });
+    // });
+
+    // ngOnDestroy() {
+    // // Cancela a assinatura ao destruir o componente
+    //   this.updateSub?.unsubscribe();
+    // }
 
 
   }
