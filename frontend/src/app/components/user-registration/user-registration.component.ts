d41a92a541a2f127104d9f64726218914b662969
vitowlabres <vitorlabres12@gmail.com>
2025-09-21 10:20:38 -0300

    ajuste na visualização do cpf

--- frontend/src/app/components/user-registration/user-registration.component.ts
+++ frontend/src/app/components/user-registration/user-registration.component.ts
@@ -110,6 +110,7 @@ export class UserRegistrationComponent implements OnInit {
   formatCpfValue(cpf: string): string {
     return cpf
       .replace(/\D/g, '')
+      .slice(0, 11)
       .replace(/(\d{3})(\d)/, '$1.$2')
       .replace(/(\d{3})(\d)/, '$1.$2')
       .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
