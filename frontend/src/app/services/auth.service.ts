60bc40951f93b775e26e2246eef875a4078e7ff9
joanakriesel <krieseljoana@gmail.com>
2025-09-22 12:46:15 -0300

    Ajuste import environment nos Services

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 import { LoginUserDTO, RegisterUserDTO } from '../models/auth.model';
+
 
 @Injectable({
   providedIn: 'root',
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 import type { User } from '../models/user.model';
 
 @Injectable({
