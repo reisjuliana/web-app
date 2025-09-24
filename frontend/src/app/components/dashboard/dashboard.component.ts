df581cecfa5de50d93d2e31dee25ab1870f89faf
vitowlabres <vitorlabres12@gmail.com>
2025-09-23 23:49:01 -0300

    ajustes nas legendas dos gráficos

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -19,7 +19,7 @@ import { NgFor } from '@angular/common';
     NgFor,
   ],
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements AfterViewInit {
   constructor(private dashboardService: DashboardService) {}
@@ -37,7 +37,7 @@ export class DashboardComponent implements AfterViewInit {
 
     this.updateBarCharts();
     this.updatePieChart();
-    this.dashboardService.getLastEntries().subscribe(entries => {
+    this.dashboardService.getLastEntries().subscribe((entries) => {
       this.lastEntries = entries;
     });
 
@@ -45,7 +45,7 @@ export class DashboardComponent implements AfterViewInit {
       this.updateBarCharts();
       this.updatePieChart();
 
-      this.dashboardService.getLastEntries().subscribe(entries => {
+      this.dashboardService.getLastEntries().subscribe((entries) => {
         this.lastEntries = entries;
       });
     });
@@ -57,39 +57,29 @@ export class DashboardComponent implements AfterViewInit {
       data: {
         labels: [],
         datasets: [
-          {
-            label: 'Consumo Médio',
-            data: [],
-            backgroundColor: '#2b891f'
-          },
+          { label: 'Consumo Médio', data: [], backgroundColor: '#2b891f' },
           {
             label: 'Quantidade em Estoque',
             data: [],
-            backgroundColor: '#4f7096'
-          }
-        ]
+            backgroundColor: '#4f7096',
+          },
+        ],
       },
       options: {
         responsive: true,
         maintainAspectRatio: false,
         plugins: {
-          legend: { display: true }
+          legend: {
+            display: true,
+            position: 'bottom', // <- aqui: legenda embaixo
+          },
         },
         backgroundColor: '#f7ffee',
         scales: {
-          x: {
-            ticks: {
-              font: {
-                size: 10 
-              }
-            }
-          },
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-
+          x: { ticks: { font: { size: 10 } } },
+          y: { beginAtZero: true },
+        },
+      },
     });
   }
 
@@ -98,24 +88,35 @@ export class DashboardComponent implements AfterViewInit {
       type: 'pie',
       data: {
         labels: [],
-        datasets: [{
-          data: [],
-          backgroundColor: [
-            '#2b891f', '#4f7096', '#e83808', '#b700ffff', '#1976d2', '#ffb300', '#8e24aa'
-          ]
-        }]
+        datasets: [
+          {
+            data: [],
+            backgroundColor: [
+              '#2b891f',
+              '#4f7096',
+              '#e83808',
+              '#b700ffff',
+              '#1976d2',
+              '#ffb300',
+              '#8e24aa',
+            ],
+          },
+        ],
       },
       options: {
         responsive: true,
         plugins: {
-          legend: { display: true }
-        }
-      }
+          legend: {
+            display: true,
+            position: 'bottom', // <- aqui: legenda embaixo
+          },
+        },
+      },
     });
   }
 
   updateBarCharts() {
-    this.dashboardService.getMetrics().subscribe(data => {
+    this.dashboardService.getMetrics().subscribe((data) => {
       this.chart.data.labels = data.labels;
       this.chart.data.datasets[0].data = data.averageConsumption;
       this.chart.data.datasets[1].data = data.stockQuantity;
@@ -124,7 +125,7 @@ export class DashboardComponent implements AfterViewInit {
   }
 
   updatePieChart() {
-    this.dashboardService.getProductQuantities().subscribe(data => {
+    this.dashboardService.getProductQuantities().subscribe((data) => {
       this.pieChart.data.labels = data.labels;
       this.pieChart.data.datasets[0].data = data.quantities;
       this.pieChart.update();
